d96fea7a48de5de624dbba9ee111fd42faf6560a
Riku Lauttia <riku.lauttia@gmail.com>
2025-02-06 16:50:41 +0200

    Fix API URLs to use Render's environment variable

--- backend/app.js
+++ backend/app.js
@@ -1,50 +1,50 @@
-const config = require("./utils/config");
-const express = require("express");
-require("express-async-errors");
-const app = express();
-const cors = require("cors");
-const logger = require("./utils/logger");
-const middleware = require("./utils/middleware");
-const mongoose = require("mongoose");
+const config = require('./utils/config')
+const express = require('express')
+require('express-async-errors')
+const app = express()
+const cors = require('cors')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
 
-const blogsRouter = require("./controllers/blogs");
-const usersRouter = require("./controllers/users");
-const loginRouter = require("./controllers/login");
+const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
-let testingRouter;
-if (process.env.NODE_ENV === "test") {
-  testingRouter = require("./controllers/testingRouter");
+let testingRouter
+if (process.env.NODE_ENV === 'test') {
+	testingRouter = require('./controllers/testingRouter')
 }
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-logger.info("connecting to", config.MONGODB_URI);
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("connected to MongoDB");
-  })
-  .catch((error) => {
-    logger.error("error connection to MongoDB:", error.message);
-  });
+	.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('error connection to MongoDB:', error.message)
+	})
 
-app.use(cors());
-app.use(express.json());
-app.use(middleware.tokenExtractor);
+app.use(cors())
+app.use(express.json())
+app.use(middleware.tokenExtractor)
 
-app.use("/api/blogs", middleware.userExtractor, blogsRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/login", loginRouter);
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
-if (process.env.NODE_ENV === "test") {
-  app.use("/api/testing", testingRouter);
+if (process.env.NODE_ENV === 'test') {
+	app.use('/api/testing', testingRouter)
 }
 
-app.get("/health", (req, res) => {
-  res.status(200).send("OK");
-});
+app.get('/health', (req, res) => {
+	res.status(200).send('OK')
+})
 
-app.use(middleware.errorHandler);
+app.use(middleware.errorHandler)
 
-module.exports = app;
+module.exports = app
--- frontend/src/services/login.js
+++ frontend/src/services/login.js
@@ -1,10 +1,12 @@
-import axios from 'axios'
+import axios from "axios";
 
-const baseUrl = '/api/login'
+const baseUrl = import.meta.env.VITE_BACKEND_URL
+  ? `${import.meta.env.VITE_BACKEND_URL}/api/login`
+  : "http://localhost:3001/api/login";
 
-const login = async credentials => {
-	const response = await axios.post(baseUrl, credentials)
-	return response.data
-}
+const login = async (credentials) => {
+  const response = await axios.post(baseUrl, credentials);
+  return response.data;
+};
 
-export default { login }
+export default { login };
