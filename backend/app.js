ad313ea5a18b84ffe577a2fe9bbb7d937dbe2303
Riku Lauttia <riku.lauttia@gmail.com>
2025-02-06 16:05:07 +0200

    added health check path to backend

--- backend/app.js
+++ backend/app.js
@@ -1,45 +1,50 @@
-const config = require('./utils/config')
-const express = require('express')
-require('express-async-errors')
-const app = express()
-const cors = require('cors')
-const logger = require('./utils/logger')
-const middleware = require('./utils/middleware')
-const mongoose = require('mongoose')
+const config = require("./utils/config");
+const express = require("express");
+require("express-async-errors");
+const app = express();
+const cors = require("cors");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+const mongoose = require("mongoose");
 
-const blogsRouter = require('./controllers/blogs')
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 let testingRouter;
-if (process.env.NODE_ENV === 'test') {
-  testingRouter = require('./controllers/testingRouter');
+if (process.env.NODE_ENV === "test") {
+  testingRouter = require("./controllers/testingRouter");
 }
 
-mongoose.set('strictQuery', false)
+mongoose.set("strictQuery", false);
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
-mongoose.connect(config.MONGODB_URI)
-	.then(() => {
-		logger.info('connected to MongoDB')
-	})
-	.catch((error) => {
-		logger.error('error connection to MongoDB:', error.message)
-	})
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
 
-app.use(cors())
-app.use(express.json())
-app.use(middleware.tokenExtractor)
+app.use(cors());
+app.use(express.json());
+app.use(middleware.tokenExtractor);
 
-app.use('/api/blogs', middleware.userExtractor, blogsRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/login', loginRouter)
+app.use("/api/blogs", middleware.userExtractor, blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
-if (process.env.NODE_ENV === 'test') {
-	app.use('/api/testing', testingRouter);
+if (process.env.NODE_ENV === "test") {
+  app.use("/api/testing", testingRouter);
 }
 
-app.use(middleware.errorHandler)
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
 
-module.exports = app
+app.use(middleware.errorHandler);
+
+module.exports = app;
